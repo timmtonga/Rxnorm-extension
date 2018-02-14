0a28645f747d3b1890853ae1b63c24afb95fdff4
timmtonga <timmtonga@gmail.com>
2018-02-14 17:49:20 -0500

    Added verification functionality

--- app/controllers/search_items_controller.rb
+++ app/controllers/search_items_controller.rb
@@ -16,15 +16,28 @@ class SearchItemsController < ApplicationController
   end
 
   def update
-    @partial = "/search_items/edit"
+
+    term = SearchItem.find(params[:id])
+    term.status = 'Verified'
+    unless (params['confirmed_matches'].blank? || params['confirmed_matches'] == 'none')
+      term.confirmed_matches = params['confirmed_matches']
+    end
+    term.save
+
+    @partial = "/batch_jobs/show"
+    @records = SearchItem.where(job_id: term.job_id, status: %w[Matched Verified])
+    @job = term.job_id
+
     respond_to do |format|
-      format.html { render root_path }
+      format.html { render '/main/index' }
       format.js {}
     end
   end
 
   def edit
     @partial = "/search_items/edit"
+    @item = SearchItem.find(params[:id])
+    @options = view_context.potential_matches(@item)
     respond_to do |format|
       format.html { render root_path }
       format.js {}
--- app/helpers/search_items_helper.rb
+++ app/helpers/search_items_helper.rb
@@ -1,2 +1,11 @@
 module SearchItemsHelper
+  def potential_matches(record)
+    results = {}
+    options = record.potential_matches.split(',') rescue []
+    (options || []).each do |option|
+      results[option] = Rxnconso.find_by_RXAUI(option).STR rescue ''
+    end
+    results['none'] = 'No Match Found'
+    return results
+  end
 end
